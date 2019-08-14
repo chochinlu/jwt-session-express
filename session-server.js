72345cf67f3325dc265c749dacce19085b0e3f6a
Cho-Ching Lu <chochinlu@gmail.com>
2019-08-14 19:47:00 +0800

    update status code.

--- session-server.js
+++ session-server.js
@@ -27,7 +27,7 @@ const PORT = 3000;
 const checkAuth = (req, res, next) => {
   // console.log(req.session);
   if (!req.session.userId) {
-    res.status(401).send('Auth fail. Please login!!');
+    res.status(403).send('Forbidden.');
   } else {
     next();
   }
